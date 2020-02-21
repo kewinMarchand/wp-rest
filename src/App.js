68aef4793d39e12443fefea0e3db9e23cb35ca0d
kewinMarchand <k.marchand@purjus.fr>
2020-02-21 20:48:00 +0100

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { useEffect, useState } from 'react';
+
+// Assets
+import logo from './logo.svg';
+
+// CSS
 import './App.css';
 
+// Components
+import Home from "./pages/Home"
+
 function App() {
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    fetch("https://wp-rest.alwaysdata.net/wp-json/")
-    .then(response => response.json())
-    .then(data => setData(data))
-  }, []);
-  
-  if (!data) {
-    return <div>Loading</div> 
-    
-  }
-  console.log(data)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{data.name}</h1>
-        <h2>{data.description}</h2>
-      </header>
-    </div>
-  );
+	// crée le state "data" et la méthode pour y injecter les données
+	const [data, setData] = useState(null)
+	
+	// au chargement du composant on va chercher les données et on les injecte dans le state
+	useEffect(() => {
+		fetch("https://wp-rest.alwaysdata.net/wp-json/")
+		.then(response => response.json())
+		.then(data => setData(data))
+	},
+	// le "[]" sert à ne jouer le fetch qu'au montage du composant, sans lui ce serait joué à chaque rendu
+	[]);
+
+	// si il n'y a pas de données on affiche le logo
+	if (!data) {
+		return  <img src={logo} className="App-logo" alt="logo" />
+	}
+
+	// sinon on affiche l'app
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>{data.name}</h1>
+				<h2>{data.description}</h2>
+			</header>
+			{/* on passe data au composant header */}
+			<Home data={data} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Home(props) {
+    // tout ça ne sert qu'à afficher le message dans la console
+    const styles =
+        "background: linear-gradient(to left, #B000B5, #BADA55);" +
+        "font-size: 40px;" +
+        "color: #FFF;" +
+        'font-family: "Comic Sans MS", "Comic Sans", cursive;' +
+        "line-height: 60px; " +
+        "padding: 0 16px;";
+    console.groupCollapsed("%c Salut Thomas !!!", styles);
+    // ici on se sert des props passées depuis App.js 
+    console.log(props.data)
+    console.groupEnd()
+    // templete de la page d'accueil
+    return (
+        <div className="Home">
+            <h3>Accueil</h3>
+            <h4>Les routes disponibles:</h4>
+            {Object.keys(props.data.routes).map(key => {
+                return (
+                    <div>
+                        <p>{key}</p>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default Home;
